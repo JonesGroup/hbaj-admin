539cfe81a58fc8ef725f7af55b2d267da1dbe383
yangyuan <1367511704@qq.com>
2020-08-26 23:54:13 +0800

    fix(初始化项目)

---
+++ src/components/search/index.vue
@@ -0,0 +1,52 @@
+<!--
+ * @Author: yangyuan
+ * @Date: 2020-04-15 23:57:13
+ * @Email: 1367511704@qq.com
+ * @LastEditTime: 2020-06-04 01:15:14
+ * @Description: 
+ -->
+<template>
+    <div class="search-content">
+        <div class="search-input" @click="toSeatch">
+            <i class="iconfont icontubiao-12"></i>
+            <span>搜索</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    methods: {
+        toSeatch() {
+            this.$router.push({ path: "/search" });
+        }
+    }
+};
+</script>
+
+<style lang="less">
+.search-content {
+    width: 100%;
+    height: 1.14rem;
+    background: #fff;
+    padding: 0.2rem 0.3rem;
+    .search-input {
+        width: 100%;
+        height: 0.58rem;
+        line-height: 0.58rem;
+        background: rgba(238, 240, 243, 1);
+        border-radius: 0.35rem;
+        padding-left: 0.26rem;
+        color: rgba(153, 153, 153, 1);
+        i {
+            font-size: 0.28rem;
+        }
+        span {
+            margin-left: 0.1rem;
+        }
+    }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,65 @@
+/*
+ * @Author: yangyuan
+ * @Date: 2020-04-14 21:26:49
+ * @Email: 1367511704@qq.com
+ * @LastEditTime: 2020-05-17 23:23:05
+ * @Description:
+ */
+import Vue from "vue";
+import Application from "./App.vue";
+import router from "./router";
+import Toast from "@/components/toast";
+import loading from "@/components/loading";
+import pageLoading from "@/components/pageLoading";
+import showModal from "@/components/showModal";
+import filters from "./filters";
+import "@/widget/skeleton";
+
+Vue.config.productionTip = false;
+
+Object.keys(filters).forEach(key => {
+    Vue.filter(key, filters[key]);
+});
+
+Vue.use(showModal);
+Vue.use(loading);
+Vue.use(pageLoading);
+Vue.use(Toast);
+
+Vue.prototype.globalConfig = window.globalConfig;
+// const metaEl = document.querySelector('meta[name="viewport"]');
+// const oldattr = metaEl.getAttribute("content");
+router.beforeEach((to, from, next) => {
+    // if (to.name == "panoEditor") {
+    //     metaEl.setAttribute("content", "width=device-width,user-scalable=no,initial-scale=" + 1 + ",maximum-scale=" + 1 + ",minimum-scale=" + 1);
+    // } else {
+    //     metaEl.setAttribute("content", oldattr);
+    // }
+
+    if (to.name == "login" && window.localStorage.getItem("authorization")) {
+        //解决登陆后 用户输入登录地址重定向到首页
+        next({ path: "/home" });
+    }
+
+    if (to.meta.requireLogin) {
+        // 是需要登录的页面
+        if (window.localStorage.getItem("authorization")) {
+            // token存在 且token没有过期
+            next();
+        } else {
+            next({ path: "/login", query: { from: location.href } });
+        }
+    } else {
+        // 不需要登录的直接next()
+        next();
+    }
+});
+
+router.afterEach(to => {
+    document.title = to.meta.title;
+});
+new Vue({
+    el: "#app",
+    router,
+    render: h => h(Application)
+});
