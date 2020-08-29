ab420076cc152752b1b9a82e2f0e72dacbc0b7a0
yangyuan <1367511704@qq.com>
2020-08-30 00:40:03 +0800

    fix(更新路由)

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,13 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-// import home from "./home"; // 主页一级模块
+import home from "./home"; // 主页一级模块
 import white from "./white"; // 白名单
 
 Vue.use(Router);
 
 // 数组需要动态挂载
-// export const routes = [home];
+export const routes = [home];
 
 // 实例
 const router = new Router({
