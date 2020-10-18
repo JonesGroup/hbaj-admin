0b67460c4283827a872022df762a8cfff84bab0a
yangyuan <1367511704@qq.com>
2020-10-18 20:07:49 +0800

    fix(tips修改)

--- src/store/tips.js
+++ src/store/tips.js
@@ -1,10 +1,20 @@
+let tips;
+
+try {
+    const local = sessionStorage.getItem("TIPS") || "";
+    tips = JSON.parse(local);
+} catch (error) {
+    tips = "";
+}
+
 const state = {
-    tips: ""
+    tips: tips || ""
 };
 
 const mutations = {
     SET_TIPS(state, tips) {
         state.tips = tips;
+        sessionStorage.setItem("TIPS", JSON.stringify(tips));
     }
 };
 
