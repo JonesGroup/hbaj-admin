7114b15a68684c263680d4f6596618e29610cff8
yangyuan <1367511704@qq.com>
2020-11-07 23:34:24 +0800

    fix: 优化列表到详情数据

--- src/router/home/CoursewareManage/children.js
+++ src/router/home/CoursewareManage/children.js
@@ -3,7 +3,8 @@ const router = [
         path: "list", // 子路由不能加/path/ 不然匹配不成功
         name: "list",
         meta: {
-            title: "项目列表"
+            title: "项目列表",
+            keepAlive: true
         },
         component: () => import("@/views/home/CoursewareManage/list/index.vue")
     },
@@ -13,7 +14,8 @@ const router = [
         meta: {
             title: "项目详情",
             hidden: true,
-            isBack: true
+            isBack: true,
+            keepAlive: false
         },
         component: () => import("@/views/home/CoursewareManage/courseDetail/index.vue")
     }
--- src/views/home/CoursewareManage/index.vue
+++ src/views/home/CoursewareManage/index.vue
@@ -1,3 +1,8 @@
 <template>
-    <router-view></router-view>
+    <div class="course">
+        <keep-alive>
+            <router-view v-if="$route.meta.keepAlive"></router-view>
+        </keep-alive>
+        <router-view v-if="!$route.meta.keepAlive"></router-view>
+    </div>
 </template>
